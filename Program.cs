dad3567616d9013f53f602620764339e6fc6121d
Cerulean96 <104350347+Cerulean96@users.noreply.github.com>
2022-05-06 21:18:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+namespace Lassons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 произвольных числа.");
+            double a = Convert.ToDouble(Console.ReadLine()), b = Convert.ToDouble(Console.ReadLine()), c = 2;
+            Console.WriteLine("Среднее арефмитическое двух введённых чисел: " + ((a + b) / c), ".");
+        }
+    }
+}
